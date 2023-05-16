0f57951c2c3a64b1b4386ffa5bb3a30d1d2b474b
RafalGontarski <gontarskirafal@gmail.com>
2023-05-16 23:00:18 +0200

    Before deploy.

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -4,7 +4,8 @@ import Work2 from '../../assets/work2.png';
 import Work3 from '../../assets/work1.png';
 import Work4 from '../../assets/work2.png';
 import Work5 from '../../assets/work1.png';
-
+import Work6 from '../../../../../../Desktop/Lesson/Portfolio/src/assets/work6.jpeg';
+import Work7 from '../../../../../../Desktop/Lesson/Portfolio/src/assets/work7.png';
 
 
 export const projectsData = [
@@ -18,24 +19,42 @@ export const projectsData = [
         id: 2,
         image: Work2,
         title: 'iroN',
-        category: 'web',
+        category: 'e-commerce',
     },
     {
         id: 3,
         image: Work3,
-        title: 'DentFlow',
-        category: 'design',
+        title: 'CodeCool Series',
+        category: 'web',
     },
     {
         id: 4,
-        image: Work4,
-        title: 'DentFlow',
-        category: 'app',
+        image: Work6,
+        title: 'RoughLike',
+        category: 'games',
     },
     {
         id: 5,
         image: Work5,
-        title: 'DentFlow',
+        title: 'AskMate',
+        category: 'web',
+    },
+    {
+        id: 6,
+        image: Work5,
+        title: 'Snake',
+        category: 'games',
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: 'Battleship',
+        category: 'games',
+    },
+    {
+        id: 8,
+        image: Work5,
+        title: 'CodeCool Shop',
         category: 'web',
     },
 ]
@@ -51,6 +70,12 @@ export const projectNav = [
         name: 'app',
     },
     {
-        name: 'design',
+        name: 'e-commerce',
+    },
+    {
+        name: 'games',
+    },
+    {
+        name: 'OOP',
     },
 ]
