e859a6aec0c09825a95ff55fb4db830c6b9e5d9c
RafalGontarski <gontarskirafal@gmail.com>
2023-05-22 18:44:10 +0200

    Testimonial component in dark mode.

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -13,13 +13,21 @@ import { Pagination } from "swiper";
 import {DarkModeContext} from "../../context/DarkModeContext";
 
 export const Testimonials = () => {
-    const { isDarkMode, toggleDarkMode } = useContext(DarkModeContext);
+    const { isDarkMode} = useContext(DarkModeContext);
     return (
         <section className="testimonials section" id="testimonials">
-            <h2 className={`section__title ${isDarkMode ? "section__title-dark-mode" : "section__title-light-mode"}`}>Recommendation</h2>
-            <span className={`section__subtitle ${isDarkMode ? "section__subtitle-dark-mode" : "section__subtitle-light-mode"}`}>Testimonial</span>
+            <h2 className={`section__title ${isDarkMode ? 
+                "section__title-dark-mode" : 
+                "section__title-light-mode"}`}
+                >Recommendation
+            </h2>
+            <span className={`section__subtitle ${isDarkMode ? 
+                "section__subtitle-dark-mode" : 
+                "section__subtitle-light-mode"}`}>
+                Testimonial
+            </span>
 
-            <Swiper className="testimonial__container"
+            <Swiper className='testimonial__container'
                     loop={true}
                     grabCursor={true}
                     slidesPerView={1}
@@ -40,12 +48,18 @@ export const Testimonials = () => {
             >
                 {Data.map(({id, image, title, description}) => {
                     return (
-                        <SwiperSlide className='testimonial__card' key={id}>
+                        <SwiperSlide className={`testimonial__card ${isDarkMode ?
+                            "testimonial__card-dark-mode" :
+                            "testimonial__card-light-mode"}`} key={id}>
                             <img src={image} alt='img'
                             className='testimonial__img' width={80} height={80}/>
 
-                            <h3 className="testimonial__name">{title}</h3>
-                            <p className="testimonial__description">{description}</p>
+                            <h3 className={`testimonial__name ${isDarkMode ?
+                                "testimonial__name-dark-mode" :
+                                ""}`}>{title}</h3>
+                            <p className={`testimonial__description ${isDarkMode ?
+                                "testimonial__description-dark-mode" :
+                                ""}`}>{description}</p>
                         </SwiperSlide>
                     )
                 })}
