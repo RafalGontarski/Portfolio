7bb6c7d2d5f35ed65af32db186aa3f6d92ef5f5a
RafalGontarski <gontarskirafal@gmail.com>
2023-05-22 18:26:46 +0200

    Work component in dark mode.

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,9 +1,10 @@
-import React, {useRef, useState} from "react";
+import React, {useContext, useRef, useState} from "react";
 import '../services/services.css';
+import {DarkModeContext} from "../../context/DarkModeContext";
 
 export const WorkItems = ({ item}) => {
     const[toggleState, setToggleState] = useState(0);
-
+    const { isDarkMode} = useContext(DarkModeContext);
     const imgRef = useRef();
 
     const openFullscreen = () => {
@@ -41,11 +42,14 @@ export const WorkItems = ({ item}) => {
         setToggleState(index);
     }
     return (
-        <div className='work__card' key={item.id}>
+        <div className={`work__card ${isDarkMode ? 
+            'work__card-dark-mode' : 'work__card-light-mode'}`} key={item.id}>
             <img src={item.image} alt='' className='work__img' onClick={() => toggleTab(1)}/>
-            <h3 className="work__title">{item.title}</h3>
-            <a href={item.url} className="work__button" >
-                Repository <i className="bx bx-right-arrow-alt work__button-icon"></i>
+            <h3 className={`work__title ${isDarkMode ?
+                'work__title-dark-mode' : 'work__title-light-mode'}`}>{item.title}</h3>
+            <a href={item.url} className={`work__button ${isDarkMode ?
+                'work__button-dark-mode' : 'work__button-light-mode'}`} >
+                Repository <i className='bx bx-right-arrow-alt work__button-icon'></i>
             </a>
 
 
@@ -53,12 +57,17 @@ export const WorkItems = ({ item}) => {
                 'work__modal active-modal' :
                 "work__modal"}>
 
-                <div className="work__modal-content">
-                    <i className="uil uil-times work__modal-close"
+                <div className={`work__modal-content ${isDarkMode ?
+                    'work__modal-content-dark-mode' : 'work__modal-content-light-mode'}`}>
+                    <i className={`uil uil-times work__modal-close ${isDarkMode ?
+                        'uil uil-times-dark-mode work__modal-close-dark-mode' : 
+                        'uil uil-times-light-mode work__modal-close-light-mode'}`}
                        onClick={() => toggleTab(0)}></i>
 
-                    <h3 className="work__modal-title">{item.title}</h3>
-                    <a href={item.url} className="work__button-modal" >
+                    <h3 className={`work__modal-title ${isDarkMode ?
+                        'work__modal-title-dark-mode' : ''}`}>{item.title}</h3>
+                    <a href={item.url} className={`work__button-modal ${isDarkMode ?
+                        'work__button-modal-dark-mode' : 'work__button-modal-light-mode'}`} >
                         Repository <i className="bx bx-right-arrow-alt work__button-icon"></i>
                     </a>
 
@@ -72,39 +81,40 @@ export const WorkItems = ({ item}) => {
 
                     <ul className="work__modal-services grid">
                         <li className="work__modal-service">
-                            <i className="uil uil-brackets-curly work__modal-icon"></i>
-                            <p className="work__modal-info">
+                            <i className={`uil uil-brackets-curly work__modal-icon ${isDarkMode ?
+                                'work__modal-icon-dark-mode' : 'work__modal-icon-light-mode'}`}></i>
+                            <p className={`work__modal-info ${isDarkMode ?
+                                'work__modal-info-dark-mode' : ''}`}>
                                 {item.technologies}
                             </p>
                         </li>
 
                         <li className="work__modal-service">
-                            <i className="uil uil-channel work__modal-icon"></i>
-                            <p className="work__modal-info">
+                            <i className={`uil uil-channel work__modal-icon ${isDarkMode ?
+                                'work__modal-icon-dark-mode' : 'work__modal-icon-light-mode'}`}></i>
+                            <p className={`work__modal-info ${isDarkMode ?
+                                'work__modal-info-dark-mode' : ''}`}>
                                 {item.frameworks}
                             </p>
                         </li>
 
                         <li className="work__modal-service">
-                            <i className="uil uil-book work__modal-icon"></i>
-                            <p className="work__modal-info">
+                            <i className={`uil uil-book work__modal-icon ${isDarkMode ?
+                                'work__modal-icon-dark-mode' : 'work__modal-icon-light-mode'}`}></i>
+                            <p className={`work__modal-info ${isDarkMode ?
+                                'work__modal-info-dark-mode' : ''}`}>
                                 {item.libraries}
                             </p>
                         </li>
 
                         <li className="work__modal-service">
-                            <i className="uil uil-database work__modal-icon"></i>
-                            <p className="work__modal-info">
+                            <i className={`uil uil-database work__modal-icon ${isDarkMode ?
+                                'work__modal-icon-dark-mode' : 'work__modal-icon-light-mode'}`}></i>
+                            <p className={`work__modal-info ${isDarkMode ?
+                                'work__modal-info-dark-mode' : ''}`}>
                                 {item.database}
                             </p>
                         </li>
-
-                        {/*<li className="work__modal-service">*/}
-                        {/*    <i className="uil uil-check-circle work__modal-icon"></i>*/}
-                        {/*    <p className="work__modal-info">*/}
-                        {/*        Web page development.*/}
-                        {/*    </p>*/}
-                        {/*</li>*/}
                     </ul>
                 </div>
             </div>
